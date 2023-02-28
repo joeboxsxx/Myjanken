4a73ba0b073c458a1f3b466acd7cdd1cf4ce12bf
joeboxsxx <joeboxsxx@ezweb.ne.jp>
2023-02-28 14:34:17 +0900

    Initial Commit

---
+++ Myjanken/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Myjanken
+//
+//  Created by 戸上健太 on 2023/02/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Myjanken/MyjankenApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyjankenApp.swift
+//  Myjanken
+//
+//  Created by 戸上健太 on 2023/02/28.
+//
+
+import SwiftUI
+
+@main
+struct MyjankenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
